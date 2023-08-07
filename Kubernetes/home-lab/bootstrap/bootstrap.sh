51f54fcd63be7428834f6d7dd0ec4c5074fd0a8b
Andrew Macdonald <andymac@nimbleapproach.com>
2023-08-07 17:38:28 +0100

    Experimenting

--- Kubernetes/home-lab/bootstrap/bootstrap.sh
+++ Kubernetes/home-lab/bootstrap/bootstrap.sh
@@ -8,4 +8,6 @@ kubectl apply -f /dev/stdin
 sops -d ./bootstrap/gh-ssh-credentials.sops.yaml |
 kubectl apply -f /dev/stdin
 
+kubectl apply -k ./flux/vars --validate=false
+
 kubectl apply -k ./bootstrap/flux --validate=false
