448a0382be8c2ede5086997589614a0fb3748d44
Andrew Macdonald <Andrew.Macdonald@texmoblank.com>
2025-09-29 14:24:07 +0100

    fdf

--- Kubernetes/home-lab/bootstrap/bootstrap.sh
+++ Kubernetes/home-lab/bootstrap/bootstrap.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
 kubectl apply -k ./bootstrap --validate=false
+echo "================="
+echo "applied bootstrap"
+echo "================="
 
 for FILE in ./flux/vars/*.sops.yaml; do
     sops -d $FILE |
         kubectl apply -f -
 done
+echo "============"
+echo "applied sops"
+echo "============"
 
 kubectl apply -k ./flux/config
+echo "============"
+echo "applied Flux"
+echo "============"
